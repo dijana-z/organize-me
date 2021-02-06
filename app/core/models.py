7e76fa9c88e5e73dee75a14976ed1b5e6407f9a6
dijana-z <dijanaz1996@gmail.com>
2021-02-06 19:52:53 +0100

    Finished user model with its tests

--- app/core/models.py
+++ app/core/models.py
@@ -2,11 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, \
                                        BaseUserManager, \
                                        PermissionsMixin
-from django.conf import settings
 
 
 class UserManager(BaseUserManager):
-
+    """Custom User Manager with helper functions."""
     def create_user(self, email, password=None, **other_fields):
         """Creates and saves a new user."""
         if not email:
@@ -15,7 +14,6 @@ class UserManager(BaseUserManager):
         if not password:
             password = self.make_random_password()
         user.set_password(password)
-
         user.save(using=self._db)
 
         return user
@@ -33,11 +31,15 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username."""
-    email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+
+    # Add user model fields
+    email = models.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
+    # Create user manager object
     objects = UserManager()
 
+    # Replace login username field with email
     USERNAME_FIELD = 'email'
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -2,6 +2,8 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+from rest_framework import status
+
 
 class AdminSiteTests(TestCase):
 
@@ -19,9 +21,23 @@ class AdminSiteTests(TestCase):
         )
 
     def test_user_listed_in_admin(self):
-        """Test that users are listed in django admin."""
+        """Test that users are listed in Django admin."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Tests that the user edit page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_user_page(self):
+        """Tests that the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
