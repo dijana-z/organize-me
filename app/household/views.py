1db754d9f2d68967da61e65c91f869c27bc62af1
dijana-z <dijanaz1996@gmail.com>
2021-02-10 00:16:52 +0100

    [Users] Added user api views, serializers and tests

--- app/household/views.py
+++ app/household/views.py
@@ -1,8 +1,6 @@
-from rest_framework import viewsets, mixins, status
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from core.models import Household
 from household import serializers
@@ -15,3 +13,4 @@ class BaseHouseholdViewset(viewsets.GenericViewSet,
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     serializer_class = serializers.HouseholdSerializer
+    queryset = Household.objects.all()
